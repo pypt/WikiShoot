c0811e979c77f777ac55911380ab3a21db5b4702
Linas V <shirshegsm@gmail.com>
2011-11-08 01:40:04 +0200

    Bureaucracy

--- WikiShoot/WSPreUploadPhotoVC.m
+++ WikiShoot/WSPreUploadPhotoVC.m
@@ -1,9 +1,17 @@
 //
-//  WSPreUploadPhotoVC.m
-//  WikiShoot
+// Copyright 2011 Linas Valiukas
 //
-//  Created by Linas Valiukas on 2011-11-08.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #import "WSPreUploadPhotoVC.h"
@@ -70,6 +78,7 @@ NSString  *const kWSPreUploadPhotoVCUploadSectionUploadCellIdentifier = @"kWSPre
 		previewImageView = [[UIImageView alloc] initWithImage:previewImage];
 		if (! previewImageView) {
 			MWERR(@"Unable to load image view for image '%@'", imagePath);
+			MW_RELEASE_SAFELY(previewImage);
 			return;
 		}
 		
